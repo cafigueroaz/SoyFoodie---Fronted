b0a811e06a112b1230028e599e6253c4ba5e5162
Cristian Figueroa <figueroaz.ca@gmail.com>
2025-10-09 18:51:32 -0500

    Se crea componente Registro. Se crea vista inicial

---
+++ src/app/features/auth/pages/register/register/register.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  imports: [CommonModule, FormsModule],
+  templateUrl: './register.html',
+  styleUrls: ['./register.scss'],
+})
+export class Register {
+  constructor(private router: Router) {}
+
+  onSubmit() {
+    console.log('Formulario de registro enviado');
+  }
+
+  goToLogin() {
+    console.log('Ir a login');
+  }
+}
