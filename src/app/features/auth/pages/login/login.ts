5db35e7d941cc9d527da884ee3e0238318d0f35f
Cristian Figueroa <figueroaz.ca@gmail.com>
2025-10-08 20:27:12 -0500

    Se crea vista de login

--- src/app/features/auth/pages/login/login.ts
+++ src/app/features/auth/pages/login/login.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   standalone: true,
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
-export class Login {}
+export class Login {
+  email = '';
+  password = '';
+
+  constructor(private router: Router, private authService: AuthService) {}
+
+  onSubmit() {
+    if (!this.email || !this.password) {
+      alert('Por favor complete ambos campos.');
+      return;
+    }
+
+    // Simulación de login (sin API)
+    const ok = this.authService.loginMock(this.email, this.password);
+    if (ok) {
+      this.router.navigate(['/home']);
+    } else {
+      alert('Credenciales inválidas (simulación).');
+    }
+  }
+}
--- src/app/features/auth/services/auth.ts
+++ src/app/features/auth/services/auth.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@angular/core';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class Auth {
-  
+@Injectable({ providedIn: 'root' })
+export class AuthService {
+  private readonly KEY = 'auth_token_demo';
+
+  // Simulación simple: acepta cualquier par no vacío
+  loginMock(a: string, b: string) {
+    return a + b;
+  }
 }
